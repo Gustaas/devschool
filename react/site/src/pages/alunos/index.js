c4807a71e3b1410e4ed8f91e92632733586d3ace
gusta <contatogustavo0743@gmail.com>
2021-09-12 13:58:25 -0300

    funcionou

--- react/site/src/pages/alunos/index.js
+++ react/site/src/pages/alunos/index.js
@@ -1,61 +1,106 @@
-
 import Cabecalho from '../../components/cabecalho'
+
 import Menu from '../../components/menu'
 
-import { useState, useEffect} from 'react';
 import { Container, Conteudo } from './styled'
 
-import Api from '../../service/api';
+import { useState, useEffect, React, useRef } from 'react'
 
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import LoadingBar from 'react-top-loading-bar'
+
+import Api from '../../service/api.js'
 const api = new Api();
 
+
 export default function Index() {
-    
     const [alunos, setAlunos] = useState([]);
     const [nome, setNome] = useState('');
     const [chamada, setChamada] = useState('');
     const [turma, setTurma] = useState('');
     const [curso, setCurso] = useState('');
     const [idAlterando, setIdAlterando] = useState(0)
+    const loading = useRef();
 
-    async function listar() {
+    async function listar () {
+
+        loading.current.continuousStart();
         let r = await api.listar();
         setAlunos(r);
+        loading.current.complete();
+
     }
 
-    async function inserir(nome, chamada, curso, turma) {
-        
-        if (idAlterando == 0) {
-            let r = await api.inserir(nome, chamada, curso, turma);
-
-            if (r.erro) alert(r.erro)
-            else alert('aluno inserido');
-        } else {
-            let r = await api.alterar(idAlterando, nome, chamada, curso, turma);
-            alert('aluno alterado');
-        }
-
-        limpar();
+    useEffect(() => { 
         listar();
-    } 
+    },
+     [])
 
-    function limpar() {
-        setNome('');
-        setChamada('');
-        setTurma('');
-        setCurso('');
-        setIdAlterando(0)
-    }
-
-    async function remover(id) {
-        let r = await api.remover(id);
-        alert('aluno removido');
-
+    async function inserir () {
+        loading.current.continuousStart();
+        if(nome !== ('') && chamada > 0 && turma !== ('') && curso !== ('')){
+            if(idAlterando === 0){
+                let r = await api.inserir(nome, chamada, curso, turma);
+                if(r.erro)
+                    toast.dark(r.erro)
+                else
+                    toast.dark('Aluno inserido!')
+            } else {
+                let r = await api.alterar(idAlterando, nome, chamada, curso, turma);
+                if (r.erro)
+                toast.dark(r.erro)
+                else
+                    toast.dark('Aluno alterado!')
+            }
+        } else (
+                toast.dark('Campos inválidos')
+        )
+        limparCampos();
         listar();
+        loading.current.complete()
     }
 
-    async function editar(item) {
+    function limparCampos () {
+        setNome('')
+        setChamada('')
+        setCurso('')
+        setTurma('')
+        setIdAlterando(0);
+    }
+    async function remover (id) {
+        loading.current.continuousStart();
+
+        confirmAlert({
+            title: 'Remover aluno',
+            message: `Tem certeza que deseja remover o aluno ${id}?`,
+            buttons: [
+                {
+                    label: 'Sim, remover',
+                    onClick: async() => {
+                        let r = await api.remover(id)
+                        if(r.erro) {
+                            toast.dark(r.erro)
+                        } else {
+                            toast.dark('Aluno removido!')
+                        }
+                        listar();
+                    }
+                },
+                {
+                    label: 'Não, Cancelar'}
+            ]
+        })
+        listar();
+
+        loading.current.complete()
+    }
+
+    async function editar (item) {
         setNome(item.nm_aluno);
         setChamada(item.nr_chamada);
         setCurso(item.nm_curso);
@@ -63,12 +108,12 @@ export default function Index() {
         setIdAlterando(item.id_matricula);
     }
 
-    useEffect(() => {
-        listar();
-    }, [] );
-    
+
     return (
+
         <Container>
+            <ToastContainer/>
+            <LoadingBar color="#EA10C7" ref={loading} />
             <Menu />
             <Conteudo>
                 <Cabecalho />
@@ -77,32 +122,32 @@ export default function Index() {
                         
                         <div class="text-new-student">
                             <div class="bar-new-student"></div>
-                            <div class="text-new-student">{ idAlterando == 0 ? "Novo Aluno" : "Alterando Aluno " + idAlterando}</div>
+                            <div class="text-new-student"> {idAlterando === 0 ? "Novo Aluno" : "Alterando aluno " + idAlterando}</div>
                         </div>
 
                         <div class="input-new-student"> 
                             <div class="input-left">
                                 <div class="agp-input"> 
                                     <div class="name-student"> Nome: </div>  
-                                    <div class="input"> <input type="text" value={nome} onChange={ e => setNome(e.target.value) } /> </div>  
+                                    <div class="input"> <input type="text" value={nome} onChange={e => setNome(e.target.value)}/> </div>  
                                 </div> 
                                 <div class="agp-input">
                                     <div class="number-student"> Chamada: </div>  
-                                    <div class="input"> <input type="text" value={chamada} onChange={ e => setChamada(e.target.value) }/> </div> 
+                                    <div class="input"> <input type="text" value={chamada} onChange={e => setChamada(e.target.value)}/> </div> 
                                 </div>
                             </div>
 
                             <div class="input-right">
                                 <div class="agp-input">
                                     <div class="corse-student"> Curso: </div>  
-                                    <div class="input"> <input type="text" value={curso} onChange={ e => setCurso(e.target.value) } /> </div>  
+                                    <div class="input"> <input type="text" value={curso} onChange={e => setCurso(e.target.value)} /> </div>  
                                 </div>
                                 <div class="agp-input">
                                     <div class="class-student"> Turma: </div>  
-                                    <div class="input"> <input type="text" value={turma} onChange={ e => setTurma(e.target.value) } /> </div> 
+                                    <div class="input"> <input type="text" value={turma} onChange={e => setTurma(e.target.value)}/> </div> 
                                 </div>
                             </div>
-                            <div class="button-create"> <button onClick={inserir}> {idAlterando == 0 ? "Cadastrar" : "alterar" } </button> </div>
+                            <div class="button-create"> <button onClick={inserir}> {idAlterando === 0 ? "Cadastrar": "Alterar"} </button> </div>
                         </div>
                     </div>
 
@@ -113,6 +158,7 @@ export default function Index() {
                         </div>
                     
                         <table class ="table-user">
+                
                             <thead>
                                 <tr>
                                     <th> ID </th>
@@ -124,22 +170,21 @@ export default function Index() {
                                     <th class="coluna-acao"> </th>
                                 </tr>
                             </thead>
-                            
                             <tbody>
 
-                                {alunos.map((item, i) =>
-                                    <tr className={ i % 2 == 0 ? "linha-alternada" : ""}>
-                                        <td> {item.id_matricula} </td>
-                                        <td title={item.nm_aluno}> {item.nm_aluno != null && item.nm_aluno.length >= 25 ? item.nm_aluno.substr(0, 25) + "..." : item.nm_aluno}</td>
-                                        <td> {item.nr_chamada} </td>
-                                        <td> {item.nm_turma} </td>
-                                        <td> {item.nm_curso} </td>
-                                        <td className="coluna-acao"> <button onClick={() => editar(item)}> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
-                                        <td className="coluna-acao"> <button onClick={() => remover(item.id_matricula)}> <img src="/assets/images/trash.svg" alt="" /> </button> </td>
-                                    </tr>  
-
-                                )}
+                            {alunos.map((item, i) =>
                                 
+                                <tr className={i % 2 === 0 ? "linha-alternada": ""}>
+                                    <td> {item.id_matricula} </td>
+                                    <td title={item.nm_aluno}> {item.nm_aluno != null && item.nm_aluno.length >= 25 ? item.nm_aluno.substr(0, 25) + '...' : item.nm_aluno}</td>
+                                    <td> {item.nr_chamada} </td>
+                                    <td> {item.nm_turma} </td>
+                                    <td> {item.nm_curso} </td>
+                                    <td className="coluna-acao"> <button onClick={ () => editar(item)}> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
+                                    <td className="coluna-acao"> <button onClick={ () => remover(item.id_matricula) }> <img src="/assets/images/trash.svg" alt="" /> </button> </td>
+                                </tr>
+
+                            )}
                             </tbody> 
                         </table>
                     </div>
